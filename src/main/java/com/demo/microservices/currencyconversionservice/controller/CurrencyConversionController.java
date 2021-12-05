1e3f0c00ede882631ba98abd037da37be396eb53
akashdeep007 <beingakscool@gmail.com>
2021-12-06 00:17:32 +0530

    Making API call using FEIGN Proxy

--- src/main/java/com/demo/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/demo/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,31 +1,45 @@
 package com.demo.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+//import java.util.HashMap;
+//
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.client.RestTemplate;
 
 import com.demo.microservices.currencyconversionservice.entity.CurrencyConverter;
+import com.demo.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConverter calculateConversionValue(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) throws Exception
 
 	{
 //		return new CurrencyConverter(10001, from, to, BigDecimal.valueOf(10), quantity, BigDecimal.valueOf(200), "");
-		HashMap<String, String> uriVariables = new HashMap<String, String>();
-		uriVariables.put("from", from);
-		uriVariables.put("to", to);
-		ResponseEntity<CurrencyConverter> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConverter.class, uriVariables);
-		CurrencyConverter currencyConverter = responseEntity.getBody();
+//		legacy way
+//		HashMap<String, String> uriVariables = new HashMap<String, String>();
+//		uriVariables.put("from", from);
+//		uriVariables.put("to", to);
+//		ResponseEntity<CurrencyConverter> responseEntity = new RestTemplate().getForEntity(
+//				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConverter.class, uriVariables);
+//		CurrencyConverter currencyConverter = responseEntity.getBody();
+//		currencyConverter.setQuantity(quantity);
+//		BigDecimal conversionMultiple = currencyConverter.getConversionMultiple();
+//		currencyConverter.setConvertedValue(quantity.multiply(conversionMultiple));
+//		return currencyConverter;
+		
+//		feign way using proxy
+		CurrencyConverter currencyConverter = proxy.retrieveExchangeValue(from, to);
 		currencyConverter.setQuantity(quantity);
 		BigDecimal conversionMultiple = currencyConverter.getConversionMultiple();
 		currencyConverter.setConvertedValue(quantity.multiply(conversionMultiple));
---
+++ src/main/java/com/demo/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.demo.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.demo.microservices.currencyconversionservice.entity.CurrencyConverter;
+
+
+@FeignClient(name = "currency-exchange" , url = "http://localhost:8000")
+public interface CurrencyExchangeProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConverter retrieveExchangeValue(@PathVariable String from, @PathVariable String to) ;
+
+}
